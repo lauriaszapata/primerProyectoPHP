01e1c1b1eb46c6de7da9e829477de8e03cc080a4
501 <501@academicos.cesde.edu.co>
2020-02-14 21:09:35 -0500

    esta es la version funcional 1 de mi codigo

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+//Esto es un comentario de linea en php
+
+/*Esto es un comentario de en parrafo en php
+Esto es un comentario de en parrafo en php
+Esto es un comentario de en parrrafo en php*/
+
+$nombre = "Laura Arias";
+echo($nombre); // echo es para imprimir en pantalla
+$edad = 26; // primero se define yluego se imprime
+
+echo($edad);
+$estatura = 1.69;
+$nombre = "Julieta Arias"; //si a una variable se le asigna otro valor nuevo se imprime el ultimo valor asigando
+echo($edad);
+echo($nombre);
+print("<br>"); // salto en linea
+echo("HOLA MUNDO");
+print("<br>"); // salto en linea
+echo("Mi Nombre es: " .$nombre. " Tengo " .$edad. " Años " );
+
+
+
+
+
+
+
+
+
+
+?>
